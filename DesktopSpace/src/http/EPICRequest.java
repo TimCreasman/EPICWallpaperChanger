3d41b191e8aa2e2eb1ba0d06374fa4017e087878
Kersplash99 <ammarabbas99@gmail.com>
2017-02-04 17:02:21 -0500

    Added EPICRequest Skeleton

---
+++ DesktopSpace/src/http/EPICRequest.java
@@ -0,0 +1,22 @@
+package http;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Class that will request both image information and the image itself
+ *
+ * @author Ammar Abbas
+ * @author Tim Creasman
+ */
+public class EPICRequest {
+
+    public static String[] getInformation(int imageNum) {
+        //TODO get informaton
+        return null;
+    }
+
+    public static BufferedImage getImage(int imageNum) {
+        //TODO get image
+        return null;
+    }
+}
